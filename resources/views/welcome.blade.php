638ec22a060d6fe8c10ac527572b61a9022391bc
Ansar Hussain <kungrani63@gmail.com>
2024-10-29 06:30:57 +0500

    change About US to Home
    h2 change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 @section('content')
  <div class="container">
     <main class="content">
-        <h2>About US</h2>
+        <h2>Home</h2>
         <p>
             Welcome to the main content section. The sidebar is positioned to the right. Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque voluptatum, necessitatibus at debitis magni dolore dolorem tempore, amet deserunt a quae voluptas unde laudantium, laboriosam quasi consectetur animi maxime natus corporis et quam? Quidem accusamus laudantium ratione optio error commodi asperiores qui enim, tempora praesentium. Repellat sunt animi doloremque sapiente cumque? Ipsam fuga impedit, reiciendis harum quod possimus voluptatibus sed delectus libero laudantium maiores. Ut eos at numquam ipsum voluptatibus, neque nihil non. Ut pariatur ipsa deserunt maiores doloremque? Quae ab fuga, accusantium blanditiis voluptas ullam facere voluptatem. Nisi officia necessitatibus dolorum eum iste in fugit non vel esse cumque!
         </p>
